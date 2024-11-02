cf84ec8fe2b075ab5b63c89465e7ad52c3459b18
LAMEfreak <leeweirong@outlook.com>
2024-11-03 04:06:39 +0800

    ESLint Import Statement Update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
+import "./globals.css";
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import "./globals.css";
 import React from "react";
 
 const geistSans = localFont({
