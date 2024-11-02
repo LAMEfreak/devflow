dd80ddcb05dfb98a03b1622d00374deb0535885c
LAMEfreak <leeweirong@outlook.com>
2024-11-03 03:31:14 +0800

    ESLint & Prettier Setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 const Home = () => {
   return (
-    <h1 className="text-3xl font-semibold">
+    <h1 className="text-3xl font-semibold text-white">
       Welcome to the world of Next.js
     </h1>
   );
