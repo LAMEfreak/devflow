c326ae61814cfe46872176bc02c76827be2a2408
LAMEfreak <leeweirong@outlook.com>
2024-11-06 01:12:22 +0800

    Create Auth Form

---
+++ components/forms/AuthForm.tsx
@@ -0,0 +1,127 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import Link from "next/link";
+import {
+  DefaultValues,
+  FieldValues,
+  Path,
+  SubmitHandler,
+  useForm,
+} from "react-hook-form";
+import { z, ZodType } from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  // FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import ROUTES from "@/constants/routes";
+
+interface AuthFormProps<T extends FieldValues> {
+  schema: ZodType<T>;
+  defaultValues: T;
+  onSubmit: (data: T) => Promise<{ success: boolean }>;
+  formType: "SIGN_IN" | "SIGN_UP";
+}
+
+const AuthForm = <T extends FieldValues>({
+  schema,
+  defaultValues,
+  formType,
+  onSubmit,
+}: AuthFormProps<T>) => {
+  const form = useForm<z.infer<typeof schema>>({
+    resolver: zodResolver(schema),
+    defaultValues: defaultValues as DefaultValues<T>,
+  });
+
+  const handleSubmit: SubmitHandler<T> = async () => {
+    // Authenticate user
+  };
+
+  const buttonText = formType === "SIGN_IN" ? "Sign In" : "Sign Up";
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(handleSubmit)}
+        className="mt-10 space-y-6"
+      >
+        {" "}
+        {Object.keys(defaultValues).map((field) => {
+          return (
+            <FormField
+              key={field}
+              name={field as Path<T>}
+              control={form.control}
+              render={({ field }) => (
+                <FormItem className="flex w-full flex-col gap-2.5">
+                  <FormLabel className="paragraph-medium text-dark400_light700">
+                    {field.name === "email"
+                      ? "Email Address"
+                      : field.name.charAt(0).toUpperCase() +
+                        field.name.slice(1)}
+                  </FormLabel>
+                  <FormControl>
+                    <Input
+                      required
+                      type={field.name === "password" ? "password" : "text"}
+                      className="paragraph-regular background-light900_dark300 light-border-2 no-focus min-h-12 rounded-1.5 border text-dark-300"
+                      placeholder="shadcn"
+                      {...field}
+                    />
+                  </FormControl>
+                  {/* <FormDescription>
+                    This is your public display name.
+                  </FormDescription> */}
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          );
+        })}
+        <Button
+          type="submit"
+          disabled={form.formState.isSubmitting}
+          className="primary-gradient paragraph-medium min-h-12 w-full rounded-2 px-4 py-3 font-sans !text-light-900"
+        >
+          {form.formState.isSubmitting
+            ? buttonText === "Sign In"
+              ? "Signing in..."
+              : "Signing Up..."
+            : buttonText}
+        </Button>
+        {formType === "SIGN_IN" ? (
+          <p>
+            Don&apos;t have an account?{" "}
+            <Link
+              href={ROUTES.SIGN_UP}
+              className="paragraph-semibold primary-text-gradient"
+            >
+              Sign up
+            </Link>
+          </p>
+        ) : (
+          <p>
+            Already have an account?{" "}
+            <Link
+              href={ROUTES.SIGN_IN}
+              className="paragraph-semibold primary-text-gradient"
+            >
+              Sign in
+            </Link>
+          </p>
+        )}
+      </form>
+    </Form>
+  );
+};
+
+export default AuthForm;
